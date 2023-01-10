de3cf3a18f9d20f678d09d153376e2f542c48d5d
Garrett Barter <garrett.barter@nrel.gov>
2023-01-10 14:06:52 -0700

    trying this one

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,4 +10,4 @@ REM See the unix build.sh for more details on the build process below.
 set MESON_ARGS=-Dincdir_numpy=%NP_INC% -Dpython_target=%PYTHON% %EXTRA_FLAGS%
 REM python -m build -n -x .
 REM pip install --prefix "%PREFIX%" --no-deps --no-index --find-links dist wisdem
-pip install --prefix "%PREFIX%" --no-deps -vv .
+%PYTHON% setup.py install --single-version-externally-managed
