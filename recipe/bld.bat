c0ce0c5f990df8dae3b5d09cbdf4bf42c4ab35d1
Garrett Barter <garrett.barter@nrel.gov>
2023-01-10 14:25:42 -0700

    try this

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,6 +8,6 @@ REM set CC_LD=link
 
 REM See the unix build.sh for more details on the build process below.
 set MESON_ARGS=-Dincdir_numpy=%NP_INC% -Dpython_target=%PYTHON% %EXTRA_FLAGS%
-REM python -m build -n -x .
-REM pip install --prefix "%PREFIX%" --no-deps --no-index --find-links dist wisdem
-%PYTHON% setup.py install --single-version-externally-managed --record=record.txt
+%PYTHON% -m build -n -x .
+%PYTHON% -m pip install --prefix "%PREFIX%" --no-deps --no-index --find-links dist wisdem
+REM %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
