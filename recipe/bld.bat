7fedc2a77adfea12df55785867cff0bb926c7145
Garrett Barter <garrett.barter@nrel.gov>
2024-01-18 17:11:55 -0700

    go back to old scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,31 +1,24 @@
 @echo on
-setlocal enabledelayedexpansion
 
-REM Set a few environment variables that are not set due to
-REM https://github.com/conda/conda-build/issues/3993
-set PIP_NO_BUILD_ISOLATION=True
-set PIP_NO_DEPENDENCIES=True
-set PIP_IGNORE_INSTALLED=True
-set PIP_NO_INDEX=True
-set PYTHONDONTWRITEBYTECODE=True
+cd %SRC_DIR%
+set NP_INC=%SP_DIR%\numpy\core\include
+REM set CC=cl
+REM set FC=flang
+REM set CC_LD=link
 
-REM see explanation here:
-REM https://github.com/conda-forge/scipy-feedstock/pull/253#issuecomment-1732578945
-set "MESON_RSP_THRESHOLD=320000"
+REM See the unix build.sh for more details on the build process below.
+set MESON_ARGS=-Dincdir_numpy=%NP_INC% -Dpython_target=%PYTHON% %EXTRA_FLAGS%
 
-REM MinGW pkgconfig doen't find anything and gets in the way of the conda one
-del /s /q %BUILD_PREFIX%\Library\mingw-w64\bin\pkg-config*
-
-REM -wnx flags mean: --wheel --no-isolation --skip-dependency-check
-%PYTHON% -m build -w -n -x .
-if %ERRORLEVEL% neq 0 (type meson_build\meson-logs\meson-log.txt && exit 1)
-
+python -m build -n -x -w .
+REM %PYTHON% -m pip install --prefix "%PREFIX%" --no-deps dist/wisdem-3.9-py3-none-any.whl
+REM %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
+REM pip install --prefix "%PREFIX%" --no-deps --no-index --find-links dist pyoptsparse
 REM `pip install dist\numpy*.whl` does not work on windows,
 REM so use a loop; there's only one wheel in dist/ anyway
 for /f %%f in ('dir /b /S .\dist') do (
     REM need to use force to reinstall the tests the second time
     REM (otherwise pip thinks the package is installed already)
-    %PYTHON% -m pip install %%f
+    %PYTHON% -m pip install --prefix "%PREFIX%" --no-deps %%f
     if %ERRORLEVEL% neq 0 exit 1
 )
 rmdir /s /q %SP_DIR%\meson_build
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
-set -ex
 
-export PIP_NO_BUILD_ISOLATION=True
-export PIP_NO_DEPENDENCIES=True
-export PIP_IGNORE_INSTALLED=True
-export PIP_NO_INDEX=True
-export PYTHONDONTWRITEBYTECODE=True
+set -eox pipefail
 
-# HACK: extend $CONDA_PREFIX/meson_cross_file that's created in
-# https://github.com/conda-forge/ctng-compiler-activation-feedstock/blob/main/recipe/activate-gcc.sh
-# https://github.com/conda-forge/clang-compiler-activation-feedstock/blob/main/recipe/activate-clang.sh
-# to use host python; requires that [binaries] section is last in meson_cross_file
-echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
 
-# meson-python already sets up a -Dbuildtype=release argument to meson, so
-# we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
-MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
+EXTRA_FLAGS=""
+NP_INC="${SP_DIR}/numpy/core/include/"
+echo "PYTHON TARGET=${PYTHON}"
 
-# -wnx flags mean: --wheel --no-isolation --skip-dependency-check
-$PYTHON -m build -w -n -x -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} || (cat meson_build/meson-logs/meson-log.txt && exit 1)
+if [ `uname` == Darwin ]; then
+    export  LDFLAGS="$LDFLAGS  -Wl,-flat_namespace,-undefined,suppress"
+fi
 
-$PYTHON -m pip install dist/wis*.whl
+if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
+  # Add pkg-config to cross-file binaries since meson will disable it
+  # See https://github.com/mesonbuild/meson/issues/7276
+  echo "[binaries]" >> "$BUILD_PREFIX"/meson_cross_file.txt
+  echo "pkg-config = '$(which pkg-config)'" >> "$BUILD_PREFIX"/meson_cross_file.txt
+  # Use Meson cross-file flag to enable cross compilation
+  EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt"
+  NP_INC=""
+fi
+
+# This is done on two lines so that the command will return failure info if it fails
+PKG_CONFIG=$(which pkg-config)
+export PKG_CONFIG
+
+
+cd "${SRC_DIR}"
+
+# MESON_ARGS is used within setup.py to pass extra arguments to meson
+# We need these so that dependencies on the build machine are not incorrectly used by meson when building for a different target
+#export MESON_ARGS="-Dincdir_numpy=${NP_INC} -Dpython_target=${PYTHON} ${MESON_ARGS}"
+export MESON_ARGS="-Dpython_target=${PYTHON} ${MESON_ARGS}"
+
+# We use this instead of pip install . because the way meson builds from within a conda-build process puts the build
+# artifacts where pip install . can't find them. Here we explicitly build the wheel into the working director, wherever that is
+# and then tell pip to install the wheel in the working directory. Also, python -m build is now the recommended way to build
+# see https://packaging.python.org/en/latest/tutorials/packaging-projects/
+python -m build -n -x -w .
+$PYTHON -m pip install --prefix "${PREFIX}" --no-deps dist/*.whl
+#pip install --prefix "${PREFIX}" --no-deps --no-index --find-links dist pyoptsparse
 bin/rm -rf ${SP_DIR}/meson_build
