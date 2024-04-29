c38acd8d51b5c8f55193c5e2ff671473c065f44f
Garrett Barter <garrett.barter@nrel.gov>
2024-04-29 13:44:05 -0600

    reverting back to old windows compilers

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -16,28 +16,28 @@ REM set FC=flang
 REM set CC_LD=link
 
 REM check if clang-cl is on path as required
-clang-cl.exe --version
-if %ERRORLEVEL% neq 0 exit 1
+REM clang-cl.exe --version
+REM if %ERRORLEVEL% neq 0 exit 1
 
 REM set compilers to clang-cl
-set "CC=clang-cl"
-set "CXX=clang-cl"
+REM set "CC=clang-cl"
+REM set "CXX=clang-cl"
 
 REM flang 17 still uses "temporary" name
-set "FC=flang-new"
+REM set "FC=flang-new"
 
 REM need to read clang version for path to compiler-rt
-FOR /F "tokens=* USEBACKQ" %%F IN (`clang.exe -dumpversion`) DO (
-    SET "CLANG_VER=%%F"
-)
+REM FOR /F "tokens=* USEBACKQ" %%F IN (`clang.exe -dumpversion`) DO (
+REM     SET "CLANG_VER=%%F"
+REM )
 
 REM attempt to match flags for flang as we set them for clang-on-win, see
 REM https://github.com/conda-forge/clang-win-activation-feedstock/blob/main/recipe/activate-clang_win-64.sh
 REM however, -Xflang --dependent-lib=msvcrt currently fails as an unrecognized option, see also
 REM https://github.com/llvm/llvm-project/issues/63741
-set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -D_MT -D_DLL --target=x86_64-pc-windows-msvc -nostdlib"
-set "LDFLAGS=--target=x86_64-pc-windows-msvc -nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld"
-set "LDFLAGS=%LDFLAGS% -Wl,-defaultlib:%BUILD_PREFIX%/Library/lib/clang/!CLANG_VER:~0,2!/lib/windows/clang_rt.builtins-x86_64.lib"
+REM set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -D_MT -D_DLL --target=x86_64-pc-windows-msvc -nostdlib"
+REM set "LDFLAGS=--target=x86_64-pc-windows-msvc -nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld"
+REM set "LDFLAGS=%LDFLAGS% -Wl,-defaultlib:%BUILD_PREFIX%/Library/lib/clang/!CLANG_VER:~0,2!/lib/windows/clang_rt.builtins-x86_64.lib"
 
 REM see explanation here:
 REM https://github.com/conda-forge/scipy-feedstock/pull/253#issuecomment-1732578945
